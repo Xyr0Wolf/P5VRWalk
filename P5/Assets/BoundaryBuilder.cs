b04703086d30abcbf90f81638b5839408ed32dff
Daniel Kierkegaard Andersen <daniel.andersen@unity3d.com>
2021-11-25 08:59:17 +0100

    BuildBoundary and Matching shader init

--- P5/Assets/BoundaryBuilder.cs
+++ P5/Assets/BoundaryBuilder.cs
@@ -1,35 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
 using Unity.XR.Oculus;
 using Unity.XR.Oculus.Input;
 using Unity.XR.OpenVR;
 using UnityEngine;
 using UnityEngine.XR;
+using Random = Unity.Mathematics.Random;
 
 namespace DefaultNamespace
 {
+    [ExecuteInEditMode]
     public class BoundaryBuilder : MonoBehaviour
     {
-        void Update()
+        NativeArray<float2> m_Points;
+        Material m_Material;
+        ComputeBuffer m_PointBuffer;
+
+        void OnEnable() => Start();
+
+        void Start()
         {
             var list = new List<XRInputSubsystem>();
             SubsystemManager.GetInstances(list);
-            var inputSubsystem = list.First(s => s.running);
-            inputSubsystem.boundaryChanged += subsystem =>
+            var inputSubsystem = list.FirstOrDefault(s => s.running);
+            if (inputSubsystem != null)
             {
-                var currentBoundaries = new List<Vector3>();
-                if (inputSubsystem.TryGetBoundaryPoints(currentBoundaries))
-                    foreach (var point in currentBoundaries)
-                        Debug.DrawLine(point,point+Vector3.up*10f);
-            };
+                inputSubsystem.boundaryChanged += FillArrayWithBoundaryPoints;
+                FillArrayWithBoundaryPoints(inputSubsystem);
+            }
             
-            //new OculusLoader().
+            if (m_Points.Length == 0)
+            {
+                m_Points = new NativeArray<float2>(100, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+                for (var i = 0; i < m_Points.Length; i++)
+                {
+                    var circle = new float2();
+                    var t = (i + 1) / (float) m_Points.Length;
+                    math.sincos(2*math.PI*t,out circle.x, out circle.y);
+                    m_Points[i] = circle*(5+noise.snoise(new float2(0,t*10)));
+                }
+            }
+
+            m_Material = GetComponent<MeshRenderer>().sharedMaterial;
             
-            // var currentBoundaries = new List<Vector3>();
-            // if (inputSubsystem.TryGetBoundaryPoints(currentBoundaries))
-            //     foreach (var point in currentBoundaries)
-            //         Debug.DrawLine(point,point+Vector3.up*10f);
+            m_PointBuffer?.Dispose();
+            m_PointBuffer = new ComputeBuffer(m_Points.Length,UnsafeUtility.SizeOf<float2>(), ComputeBufferType.Structured);
+            m_PointBuffer.SetData(m_Points);
+            Shader.SetGlobalBuffer("points", m_PointBuffer);
         }
+
+        void FillArrayWithBoundaryPoints(XRInputSubsystem inputSubsystem)
+        {
+            // Create Points Array
+            var currentBoundaries = new List<Vector3>();
+            if (inputSubsystem.TryGetBoundaryPoints(currentBoundaries))
+                if(m_Points.IsCreated)
+                    m_Points.Dispose();
+            m_Points = new NativeArray<float2>(currentBoundaries.Select(v => new float2(v.x, v.z)).ToArray(), Allocator.Persistent);
+            
+            // Create Points Compute Buffer
+            m_PointBuffer?.Dispose();
+            m_PointBuffer = new ComputeBuffer(m_Points.Length,UnsafeUtility.SizeOf<float2>());
+            m_PointBuffer.SetData(m_Points);
+            m_Material.SetBuffer("points", m_PointBuffer);
+        }
+
+        void Update()
+        {
+            
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            var points = new float2[m_PointBuffer.count];
+            m_PointBuffer.GetData(points);
+            for (var i = 0; i < points.Length; i++)
+            {
+                var pointV = new Vector3(points[i].x, 0, points[i].y);
+                Gizmos.color = Color.HSVToRGB(i / (float) points.Length, 1, 1);
+                Gizmos.DrawLine(pointV,pointV+Vector3.up*5f);
+                
+                var pointNext = points[(i+1)%points.Length];
+                var pointVNext = new Vector3(pointNext.x, 0, pointNext.y);
+                Gizmos.DrawLine(pointV+Vector3.up,pointVNext+Vector3.up);
+            }
+        }
+
+        void OnDisable() => m_PointBuffer?.Dispose();
     }
 }
---
+++ P5/Assets/DrawWallsOnSand.shader
@@ -0,0 +1,91 @@
+﻿Shader "Custom/DrawWallsOnSand"
+{
+    Properties
+    {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags
+        {
+            "RenderType"="Opaque" "Queue"="Geometry+1" "ForceNoShadowCasting" = "True"
+        }
+        Offset -1, -1
+        blend srcalpha OneMinusSrcAlpha
+
+        pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float4 world_space_pos : POSITION1;
+            };
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.world_space_pos = mul(unity_ObjectToWorld, v.vertex);
+                o.vertex = mul(unity_MatrixVP, o.world_space_pos);
+                return o;
+            }
+
+            #ifdef SHADER_API_D3D11
+            StructuredBuffer<float2> points : register(t1);
+            #endif
+
+            fixed4 frag(v2f input) : SV_Target
+            {
+                float val = 0;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    val += saturate(1-2*distance(points[i], input.world_space_pos.xz));
+                }
+                
+                return val;
+            }
+            ENDCG
+        }
+        //        
+        //        // Physically based Standard lighting model, and enable shadows on all light types
+        //        //#pragma surface surf Standard decal:blend
+        //        //#pragma target 3.5
+        //
+        //        struct Input {
+        //            float2 uv_MainTex;
+        //            float3 worldPos;
+        //        };
+        //
+        //        sampler2D _MainTex;
+        //        #ifdef SHADER_API_D3D11
+        //        StructuredBuffer<float2> points : register(t1);
+        //        #endif
+        //        
+        //        void surf (Input IN, inout SurfaceOutputStandard o) {
+        //            // Albedo comes from a texture tinted by color
+        //            const float2 uv = IN.worldPos.xz;
+        //            float4 pointA = 0;
+        //            //const float dist = distance(IN.worldPos.xz, points[20]);
+        //            float4 col = 1;
+        //
+        //            #ifdef SHADER_API_D3D11
+        //            col = float4(uv,0,1);
+        //            #endif
+        //            
+        //            o.Albedo = col.rgb;
+        //            o.Alpha = col.a;
+        //        }
+        //        
+        //        ENDCG
+    }
+    FallBack "Diffuse"
+}
